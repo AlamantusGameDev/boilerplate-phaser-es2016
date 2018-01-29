6c7d31d1a6c2ef40d36a3c6ae4d4b23b74ebb4d4
Robbie Antenesse <gamedev@alamantus.com>
2018-01-29 15:14:59 -0700

    Remove misguided comma-first styling.

--- app/models/guis/main-screen.js
+++ app/models/guis/main-screen.js
@@ -9,22 +9,22 @@ export class MainScreen extends UI {
         let game = this.state.game;
 
         let mainText = this.addText(game.width / 2, game.height * 0.25, 'Phaser Example', {
-            font: 'ExampleFont'
-        ,   size: 48
+            font: 'ExampleFont',
+            size: 48,
         });
 
         let mainButton = this.addTextButton(game.width / 2, game.height / 2, 'Play', () => {
             this.changeState('Example');
         }, {
-            fontSize: 20
-        ,   buttonRadius: {
-                tl: 15
-            ,   br: 15
-            }
-        ,   buttonPaddingScale: {
-                vertical: 0.1
-            ,   horizontal: 0.3
-            }
+            fontSize: 20,
+            buttonRadius: {
+                tl: 15,
+                br: 15,
+            },
+            buttonPaddingScale: {
+                vertical: 0.1,
+                horizontal: 0.3,
+            },
         });
     }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -3,100 +3,98 @@
 // Change ENV to 'prod' to enable minify
 const ENV = 'dev';
 
-const path = require('path')
-,   webpack = require('webpack')
-;
+const path = require('path'),
+    webpack = require('webpack');
 
-const phaserModulePath = path.join(__dirname, '/node_modules/phaser/')
-,   phaserPath = path.join(phaserModulePath, 'build/custom/phaser-split.js')
-,   pixiPath = path.join(phaserModulePath, 'build/custom/pixi.js')
-,   p2Path = path.join(phaserModulePath, 'build/custom/p2.js')
-;
+const phaserModulePath = path.join(__dirname, '/node_modules/phaser/'),
+    phaserPath = path.join(phaserModulePath, 'build/custom/phaser-split.js'),
+    pixiPath = path.join(phaserModulePath, 'build/custom/pixi.js'),
+    p2Path = path.join(phaserModulePath, 'build/custom/p2.js');
 
 let webpackExport = {
     entry: {
-        game: './app/main'
-    // ,   optionalOtherBundle: './path/to/other/script'
-    }
-
-,   output: {
-        path: './public/dist'
-    ,   filename: '[name].bundle.js'
-    }
-
-,   module: {
+        game: './app/main',
+        // optionalOtherBundle: './path/to/other/script'
+    },
+    
+    output: {
+        path: './public/dist',
+        filename: '[name].bundle.js',
+    },
+    
+    module: {
         loaders: [
             {
-                test: /phaser\.js$/
-            ,   loader: 'script-loader'
-            }
-        ,   {
-                test: /\.js$/
-            ,   exclude: /node_modules/
-            ,   loader: 'babel-loader'
-            }
-        ,   {
-                test: /\.(json)$/
-            ,   loader: 'json-loader'
-            }
-        ,   {
-                test: /\.(mp3|wav|ogg)$/
-            ,   loader: 'url-loader?name=./assets/audio/[name].[ext]?[hash]'
-            }
-        ,   {
-                test: /\.(jpe?g|png)$/
-            ,   loaders: [
-                    'url-loader?limit=25000&name=./assets/images/[name].[ext]?[hash]'
-                ,   'img-loader?progressive=true&optimizationLevel=5'
+                test: /phaser\.js$/,
+                loader: 'script-loader',
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.(json)$/,
+                loader: 'json-loader',
+            },
+            {
+                test: /\.(mp3|wav|ogg)$/,
+                loader: 'url-loader?name=./assets/audio/[name].[ext]?[hash]',
+            },
+            {
+                test: /\.(jpe?g|png)$/,
+                loaders: [
+                    'url-loader?limit=25000&name=./assets/images/[name].[ext]?[hash]',
+                    'img-loader?progressive=true&optimizationLevel=5',
                 ]
-            }
-        ,   {
-                test: /\.css$/
-            ,   loaders: [
-                    'style-loader'
-                ,   'css-loader'
+            },
+            {
+                test: /\.css$/,
+                loaders: [
+                    'style-loader',
+                    'css-loader',
                 ]
-            }
-        ,   {
-                test: /\.woff$/
-            ,   loader: 'url-loader?limit=65000&mimetype=application/font-woff&name=./assets/fonts/[name].[ext]'
-            }
-        ,   {
-                test: /\.woff2$/
-            ,   loader: 'url-loader?limit=65000&mimetype=application/font-woff2&name=./assets/fonts/[name].[ext]'
-            }
-        ,   {
-                test: /\.[ot]tf$/
-            ,   loader: 'url-loader?limit=65000&mimetype=application/octet-stream&name=./assets/fonts/[name].[ext]'
-            }
-        ,   {
-                test: /\.eot$/
-            ,   loader: 'url-loader?limit=65000&mimetype=application/vnd.ms-fontobject&name=./assets/fonts/[name].[ext]'
-            }
+            },
+            {
+                test: /\.woff$/,
+                loader: 'url-loader?limit=65000&mimetype=application/font-woff&name=./assets/fonts/[name].[ext]',
+            },
+            {
+                test: /\.woff2$/,
+                loader: 'url-loader?limit=65000&mimetype=application/font-woff2&name=./assets/fonts/[name].[ext]',
+            },
+            {
+                test: /\.[ot]tf$/,
+                loader: 'url-loader?limit=65000&mimetype=application/octet-stream&name=./assets/fonts/[name].[ext]',
+            },
+            {
+                test: /\.eot$/,
+                loader: 'url-loader?limit=65000&mimetype=application/vnd.ms-fontobject&name=./assets/fonts/[name].[ext]',
+            },
         ]
-    }
-
-,   plugins: [
+    },
+    
+    plugins: [
         new webpack.ProvidePlugin({
-            PIXI: pixiPath
-        ,   p2: p2Path
-        ,   Phaser: phaserPath
-        })
-    ]
+            PIXI: pixiPath,
+            p2: p2Path,
+            Phaser: phaserPath,
+        }),
+    ],
 };
 
 if (ENV === 'prod') {
     webpackExport.plugins.push(
         new webpack.optimize.UglifyJsPlugin({
             compress: {
-                warnings: false
-            }
-            ,   mangle: {
-                except: ['webpackJsonp']
-            }
-            ,   output: {
-                comments: false
-            }
+                warnings: false,
+            },
+            mangle: {
+                except: ['webpackJsonp'],
+            },
+            output: {
+                comments: false,
+            },
         })
     );
 } else {
